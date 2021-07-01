36b58884ecca61f9ba9afd8b221653ec3b83fc68
salishoma <abubakaroma91@yahoo.com>
2021-07-01 12:18:35 +0100

    Add security to application

---
+++ src/main/java/com/connect/connectingpeople/security/WebSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.connect.connectingpeople.security;
+
+import com.connect.connectingpeople.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.crypto.SecretKey;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+    private final UsersService usersService;
+    private final Environment environment;
+    private final SecretKey secretKey;
+
+    @Autowired
+    public WebSecurityConfig(PasswordEncoder passwordEncoder,
+                             UsersService usersService,
+                             Environment environment,
+                             SecretKey secretKey){
+        this.passwordEncoder = passwordEncoder;
+        this.usersService = usersService;
+        this.environment = environment;
+        this.secretKey = secretKey;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,
+                        environment.getProperty("login.url.path"), "/api/users/").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilter(getAuthenticationFilter())
+                .addFilterAfter(new JwtTokenVerifier(secretKey, environment), AuthenticationFilter.class);
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager(), secretKey);
+        authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersService).passwordEncoder(passwordEncoder);
+    }
+}
